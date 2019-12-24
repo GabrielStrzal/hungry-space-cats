c1d4a15f0e7fe24521d736f5fbb50d6c650757a6
Gabriel <gabrielstrzal@gmail.com>
2019-12-24 09:45:35 -0400

    changing the version of Lwjgl to 3

--- desktop/src/com/strzal/hungry/desktop/DesktopLauncher.java
+++ desktop/src/com/strzal/hungry/desktop/DesktopLauncher.java
@@ -1,12 +1,14 @@
 package com.strzal.hungry.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.strzal.hungry.HungrySpaceCats;
+import com.strzal.hungry.config.GameConfig;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new HungrySpaceCats(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration ();
+		config.setWindowedMode( (int)GameConfig.SCREEN_WIDTH, (int)GameConfig.SCREEN_HEIGHT);
+		new Lwjgl3Application(new HungrySpaceCats(), config);
 	}
 }
